e28c67b530a1a196d74543d79c92986bc6b80605
faith-thedev <faithshubs11@gmail.com>
2025-09-13 14:50:04 +0100

    updated cors route

--- src/server.ts
+++ src/server.ts
@@ -18,7 +18,7 @@ const app = express();
 const server = createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://minor-seat.pipeops.net",
     methods: ["GET", "POST"],
   },
 });
